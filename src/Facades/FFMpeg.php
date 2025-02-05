730f7e290b666141522e94e50909e29475734920
lambq <lisongxia@vip.qq.com>
2025-02-05 16:11:31 +0800

    lambq update

---
+++ src/Facades/FFMpeg.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lambq\LaravelFfmpeg\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class FFMpeg extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'LaravelFfmpeg';
+    }
+}
